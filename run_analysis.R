69d000e9791c090f9cecba8b82e8b79518e3ffb9
John Bedoya <alrabef@gmail.com>
2016-09-11 15:44:58 -0300

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -31,53 +31,6 @@ library(dplyr)
 library(data.table)
 
 ## Modifygin the names of the variables in the feature file
-## The names are changed following the code:
-##  Body -> B
-##  Gyro -> Gy
-##  Gravity -> Gr
-##  Acc -> A
-##  Jerk -> J
-##  Mag _> M
-#
-# tBodyAcc
-# tGravityAcc
-# tBodyAccJerk
-# tBodyGyro
-# tBodyGyroJerk
-# tBodyAccMag
-# tGravityAccMag
-# tBodyAccJerkMag
-# tBodyGyroMag
-# tBodyGyroJerkMag
-# fBodyAcc-XYZ
-# fBodyAccJerk-XYZ
-# fBodyGyro-XYZ
-# fBodyAccMag
-# fBodyAccJerkMag
-# fBodyGyroMag
-# fBodyGyroJerkMag 
-#
-# the other values as, 
-#
-# mean(): Mean value
-# std(): Standard deviation
-# mad(): Median absolute deviation 
-# max(): Largest value in array
-# min(): Smallest value in array
-# sma(): Signal magnitude area
-# energy(): Energy measure. Sum of the squares divided by the number of values. 
-# iqr(): Interquartile range 
-# entropy(): Signal entropy
-# arCoeff(): Autorregresion coefficients with Burg order equal to 4
-# correlation(): correlation coefficient between two signals
-# maxInds(): index of the frequency component with largest magnitude
-# meanFreq(): Weighted average of the frequency components to obtain a mean frequency
-# skewness(): skewness of the frequency domain signal 
-# kurtosis(): kurtosis of the frequency domain signal 
-# bandsEnergy(): Energy of a frequency interval within the 64 bins of the FFT of each window.
-# angle(): Angle between to vectors.
-# 
-# are leave unchanged, in the same way as the levels of the factors, which contains the original names of the variables.
 
 feature_mod <- read.table("features.txt")[,2]
 
